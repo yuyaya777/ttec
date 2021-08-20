7539beaf9184e75d48dcd513b9a5c77154a094c9
yuyaya777 <yuyaya777@gmail.com>
2021-08-20 08:53:26 +0000

    deviseのルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
   root to: 'homes#top'
 end
