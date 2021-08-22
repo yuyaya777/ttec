0c68f3b594db3f587bdc1234cbe38f708df824b0
yuyaya777 <yuyaya777@gmail.com>
2021-08-22 09:28:30 +0000

    カスタマーテーブル項目追加

--- db/migrate/20210816121316_devise_create_customers.rb
+++ db/migrate/20210816121316_devise_create_customers.rb
@@ -31,8 +31,16 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-      
-      
+
+      t.string :last_name
+      t.string :first_name
+      t.string :customer_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postal_code
+      t.text :address
+      t.string :phone_number
+      t.boolean :customer_status
 
 
       t.timestamps null: false
